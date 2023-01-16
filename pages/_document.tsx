4a92daccd71e65835758ba378240fa73caedfb59
earn-to-sleep <o9o.email@gmail.com>
2023-01-16 17:19:57 +0900

    google analytics with component pages

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,20 +6,6 @@ export default function Document() {
     <Html lang="ja">
       <Head />
       <Script src="../node_modules/flowbite/dist/flowbite.min.js" />
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-ZQ1FTYHH7G" />
-      <Script
-        id="gtag-init"
-        strategy="afterInteractive"
-        dangerouslySetInnerHTML={{
-          __html: `
-           window.dataLayer = window.dataLayer || [];
-           function gtag(){dataLayer.push(arguments);}
-           gtag('js', new Date());
-         
-           gtag('config', 'G-ZQ1FTYHH7G');
-           `,
-        }}
-      />
       <body>
         <Main />
         <NextScript />
--- public/gtag.js
+++
@@ -1,6 +0,0 @@
-import "https://www.googletagmanager.com/gtag/js?id=G-ZQ1FTYHH7G"
-window.dataLayer = window.dataLayer || [];
-function gtag() { dataLayer.push(arguments); }
-gtag('js', new Date());
-
-gtag('config', 'G-ZQ1FTYHH7G');
