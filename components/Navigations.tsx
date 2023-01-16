fa5899478f6f53e8d1e64866438d367f47d3419e
earn-to-sleep <o9o.email@gmail.com>
2023-01-16 15:53:59 +0900

    delete slug

--- components/Navigations.tsx
+++ components/Navigations.tsx
@@ -1,10 +1,12 @@
+import Image from "next/image"
 import Link from "next/link"
+
 export default function Navigations() {
     return (
         <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900">
             <div className="container flex flex-wrap items-center justify-between mx-auto">
                 <Link href="/" className="flex items-center">
-                    <img src="/logo.svg" className="h-6 mr-3 sm:h-9" width={24} height={24} alt="Flowbite Logo" />
+                    <Image src="/logo.svg" className="h-6 mr-3 sm:h-9" width={24} height={24} alt="Logo" />
                     <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">User596E9F4</span>
                 </Link>
                 <button data-collapse-toggle="navbar-default" type="button" className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-default" aria-expanded="false">
